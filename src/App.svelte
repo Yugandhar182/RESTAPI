42a367c1d728e6ff2a5f437c17cb6d8b603878b2
Yugandhar182 <yugandhar@recruitly.io>
2023-05-16 12:09:54 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,70 @@
 <script>
-	import Grid from "gridjs-svelte";
-	
-	
-		const data = [
-	  { name: "Sachin", runs: 320000, age: 40, centuries: 100,  wickets: 168 },
-	  { name: "Kohli", runs: 25000, age: 34, centuries: 75, wickets: 78 },
-	  { name: "Root", runs: 19000, age: 33, centuries: 45, wickets: 98 },
-	  { name: "Smith", runs: 17000, age: 32, centuries: 41, wickets: 119 },
-	  { name: "Pointing", runs: 22000, age: 37, centuries: 65, wickets: 68 },
-	  { name: "Kane", runs: 18000, age: 37, centuries: 35, wickets: 61 },
-	  { name: "Anderson", runs: 2000, age: 39, centuries: 1, wickets: 678 },
-	  { name: "Warne", runs: 1800, age:39, centuries: 2, wickets: 778 },
-	];
-  
-	  
+  import { onMount } from "svelte";
 
-	const columns = ["name", "runs" , "age" , "centuries" , "Wickets"];
-	
-  
+  const endpoint = "https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3";
+  let data = [];
 
-  </script>
-  <Grid data={data} 
-   sort 
-   search 
-   pagination={{ enabled: true, limit:5 }}  />
+  onMount(async function() {
+    try {
+      const response = await fetch(endpoint);
+      data = await response.json();
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  });
+</script>
 
-  <style global>
-  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-  </style>
-  
+<main>
+  <h1>Click below</h1>
+  <p><a href="https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3">fetching the information</a></p>
+
+  {#if data.length > 0}
+    <table>
+      <thead>
+        <tr>
+          {#each Object.keys(data[0]) as key}
+            <th>{key}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each data as item}
+          <tr>
+            {#each Object.values(item) as value}
+              <td>{value}</td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+  }
+
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th,
+  td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+</style>
