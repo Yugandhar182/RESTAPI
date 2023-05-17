8fe8163dc3648e87f0db4967a45f07f2790675b1
Yugandhar182 <yugandhar@recruitly.io>
2023-05-17 20:26:35 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
   }
 </script>
 
-<main>
+<main class="container mt-4">
   {#if tableVisible}
     <Grid
       search
@@ -28,11 +28,13 @@
         title: item.title,
         reference: item.reference,
         status: item.status,
-        industry: item.industry
+        industry: item.industry,
+        customData: item.customData  // Add the additional property here
       }))} />
   {/if}
 </main>
 
 <style global>
   @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  @import "https://cdn.jsdelivr.net/npm/bootstrap@5.7.2/dist/css/bootstrap.min.css";
 </style>
