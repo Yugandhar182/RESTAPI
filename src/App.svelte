6ae9862127db1933bd1bc73b4284ac7df95c5530
Yugandhar182 <yugandhar@recruitly.io>
2023-05-17 20:16:37 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
   import { onMount } from "svelte";
-  
+  import Grid from "gridjs-svelte";
 
   let jsonData = [];
- 
+  let tableVisible = false;
 
   onMount(async () => {
     const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
@@ -11,58 +11,36 @@
     jsonData = responseData.data;
   });
 
+  function openTable() {
+    if (!tableVisible) {
+      fetchData();
+    }
+  }
+
+  async function fetchData() {
+    const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
+    const responseData = await response.json();
+    jsonData = responseData.data;
+    tableVisible = true;
+  }
 </script>
 
+<button on:click={openTable}>Display Table</button>
+
 <main>
-  {#if jsonData.length > 0}
-    <div class="grid-container">
-      {#each jsonData as item}
-        <div class="grid-item">
-          <span class="heading">id:</span>
-          <span>{item.id}</span>
-        </div>
-        <div class="grid-item">
-          <span class="heading">reference:</span>
-          <span>{item.reference}</span>
-        </div>
-        <div class="grid-item">
-          <span class="heading">status:</span>
-          <span>{item.status}</span>
-        </div>
-        <div class="grid-item">
-          <span class="heading">industry:</span>
-          <span>{item.industry}</span>
-        </div>
-      {/each}
-    </div>
-   
+  {#if tableVisible}
+    <Grid
+      search
+      sort
+      pagination={{ enabled: true, limit: 38 }}
+      data={jsonData.map(item => ({
+        title: item.title,
+        reference: item.reference,
+        status: item.status
+      }))} />
   {/if}
 </main>
 
-<style>
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-  }
-
-  .grid-container {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* Four columns */
-    gap: 8px;
-    width: 100%;
-  }
-
-  .grid-item {
-    border: 1px solid #ddd;
-    padding: 5px;
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .heading {
-    font-weight: bold;
-  }
+<style global>
+  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
 </style>
